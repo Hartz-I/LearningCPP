7a0e17fe9f80783e50098061d82f3cf055a73e43
Ahnaf Sadik Inan <ahnafsadikinan@gmail.com>
2021-05-11 06:41:54 -0700

    learned to print

--- TripleX.cpp
+++ TripleX.cpp
@@ -1,5 +1,11 @@
+//adding liabray : collection of code written by other players
+//preprocessor directive #include
+#include <iostream> //header file
+
+
 int main() //int means type int. int mains means the main named function returns a integer.
 { //function body
+    std::cout << "Hello World!!!" ; //std = standard ; is a name space, :: = scope operator ; whats in namespace, cout is whats defined in namespace, << output the right, "" within them string, end with ;
     return 0; //return is used as exit status
 }
 
