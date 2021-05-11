0541a0f9f2f8a780d9999031ecca7b0aa30c59fd
Ahnaf Sadik Inan <ahnafsadikinan@gmail.com>
2021-05-11 06:25:59 -0700

    trying out code

---
+++ TripleX.cpp
@@ -0,0 +1,7 @@
+int main() //int means type int. int mains means the main named function returns a integer.
+{ //function body
+    return 0; //return is used as exit status
+}
+
+//int main() {return 0;}  this also work
+//compiler ignores whitespace
