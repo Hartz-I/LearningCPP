adb365e59ef10cbfc8cc239e626b38401db1aacd
Ahnaf Sadik Inan <ahnafsadikinan@gmail.com>
2021-05-23 03:37:09 -0700

    learned conditionals and functions

--- TripleX.cpp
+++ TripleX.cpp
@@ -5,7 +5,72 @@
 
 int main() //int means type int. int mains means the main named function returns a integer.
 { //function body
-    std::cout << "Hello World!!!" ; //std = standard ; is a name space, :: = scope operator ; whats in namespace, cout is whats defined in namespace, << output the right, "" within them string, end with ;
+    //std::cout << "Hello World!!!" ; //std = standard ; is a name space, :: = scope operator ; whats in namespace, cout is whats defined in namespace, << output the right, "" within them string, end with ;
+    
+    std:: cout << "In the land of mathlocus there lies the mythic city of zeros. You are a prisoner in the central jail of zeros living your life meditating while amending your sins.";
+    std:: cout << std::endl; //end line function in std namespace
+    std:: cout << "Though you are supposed to spend rest of your life there, the devines are pleased with your worship. So you are given an opportunity to flee the prison. what will you do?" << std:: endl;
+    std:: cout << "You choose YES! Therefore given a puzzle to solve...";
+
+    //c = 10; can't assign before declaring
+
+    //variable names should start with capital letters
+    const int CodeA = 4; //declaring variables
+    const int CodeB = 5;
+    const int CodeC = 6;
+
+    const int d = 4; //fixes a value that can't be changed
+    //d = 5; won't work
+    
+    //assigning new value
+    //a = 7 ; //= is assignment operator
+
+    /*
+    This 
+    is a
+    multi line
+    comment
+    */
+
+    int CodeSum = CodeA + CodeB + CodeC; // - , /, * can also be used
+    int CodeProduct  = CodeA * CodeB * CodeC;
+
+    //b = 8;
+
+    std:: cout << std::endl; //other wise goes with previous line
+    //std:: cout << a;
+
+    std:: cout << std:: endl; //or simply the \n
+    //std:: cout << a + b + c;
+    std:: cout << "There are three numbers in the lock! \n";
+    std:: cout << "and they add up to: " << CodeSum; //using this to get new line instead of new call line
+
+    //std:: cout << std::endl; 
+    std:: cout <<"\n The codes multiply to give: "<< CodeProduct << std::endl;
+
+    int GuessA, GuessB, GuessC;
+
+    //giving an input
+    std:: cin >> GuessA; //character input
+    std:: cin >> GuessB;
+    std:: cin >> GuessC;// or put all in same line
+
+    //the input can be put in same line with space thoug with different input
+    //std:: cout << "You entered: " << GuessA << GuessB << GuessC;
+    //bug anything other than a number will become 0
+
+    //this is for comparing the guesses
+    int GuessSum = GuessA + GuessB + GuessC;
+    int GuessProduct = GuessA * GuessB * GuessC;
+
+    //compare with if statements
+    if (GuessSum == CodeSum && GuessProduct == CodeProduct)
+    {
+        std:: cout << "You have entered the correct answer!";
+    } else {
+        std:: cout << "You have failed and died!";
+    }
+
     return 0; //return is used as exit status
 }
 
--- trial/TripleX.cpp
+++ trial/TripleX.cpp
@@ -1,7 +1,51 @@
+//preprocessor directive
 #include <iostream>
 
 int main() 
 {
-    std:: cout << "Hello World!";
-    return 0;
-} 
+    //std:: cout << "Hello World!";
+
+    //expression statements
+    // Print the welcome massages to terminal
+    std:: cout << "In the land of mathlocus there lies the mythic city of zeros. You are a prisoner in the central jail of zeros living your life meditating while amending your sins. \n";
+    //std:: cout << std::endl;
+    std:: cout << "Though you are supposed to spend rest of your life there, the devines are pleased with your worship. So you are given an opportunity to flee the prison. what will you do? \n"; //<<std:: endl;
+
+    //declaration statements
+    //declare 3 number code
+    const int CodeA = 4;
+    const int CodeB = 5;
+    const int CodeC = 6;
+
+    const int CodeSum = CodeA + CodeB + CodeC;
+    const int CodeProduct = CodeA * CodeB * CodeC;
+
+    //expression statements
+    //Print CodeSum and product on the terminal
+    std:: cout << std:: endl;
+    std:: cout << "+ There are 3 numbers in the lock! \n"; //<< std:: endl;
+    std:: cout << "+ The code add-up to: " <<CodeSum ;
+    std:: cout <<"\n+ The codes multiply to give: "<< CodeProduct << std:: endl;
+
+    int GuessA, GuessB, GuessC;
+
+    //giving an input
+    //store player guess
+    std:: cin >> GuessA >> GuessB >> GuessC;
+
+    int GuessSum = GuessA + GuessB + GuessC;
+    int GuessProduct = GuessA * GuessB * GuessC;
+
+    //Check if the player ans is correct
+    if (GuessSum == CodeSum && GuessProduct == CodeProduct)
+    {
+        std:: cout << "You have entered the correct answer!";
+    }
+
+    else 
+    {
+        std:: cout << "You have failed and died!";
+    }
+
+    return 0; //return statement
+}
